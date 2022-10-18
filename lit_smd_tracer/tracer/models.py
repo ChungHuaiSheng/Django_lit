29d01f36ba96c30c4c32e4facc5ff3a30ed48299
Huai Sheng Chung <s15623@gmail.com>
2022-10-19 00:05:42 +0800

    [Modify] models

--- lit_smd_tracer/tracer/models.py
+++ lit_smd_tracer/tracer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+
 
 # Create your models here.
 class Path(models.Model):
@@ -6,6 +8,19 @@ class Path(models.Model):
     reticle_id = models.CharField(max_length=10)
     eqp_id = models.CharField(max_length=6)
     lot_id = models.CharField(max_length=8)
+    wafer_id = models.CharField(max_length=8)
+    create_dt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.wafer_id
+
 
 class Tracer(models.Model):
     trace_path = models.ForeignKey(Path, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.trace_path
+
+
+class PathAdmin(admin.ModelAdmin):
+    list_display = ('id', 'wafer_id')
--- lit_smd_tracer/vendor/models.py
+++ lit_smd_tracer/vendor/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+
 
 # Create your models here.
 class Vendor(models.Model):
@@ -7,8 +9,18 @@ class Vendor(models.Model):
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.vendor_name
+
+
 class Food(models.Model):
     food_name = models.CharField(max_length=30)
     price_name = models.DecimalField(max_digits=3, decimal_places=0)
     food_vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.food_name
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vendor_name')
